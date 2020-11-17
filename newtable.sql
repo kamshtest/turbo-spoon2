56593f7e7040be6bcd04cb5549da7dbfcfbf1d40
gurushyam7 <61306115+gurushyam7@users.noreply.github.com>
2020-11-17 15:07:25 +0530

    Add files via upload

---
+++ newtable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TOP_SALESFORCE_ACCOUNTS(
+   user_id serial PRIMARY KEY,
+   username VARCHAR (50) UNIQUE NOT NULL,
+   password VARCHAR (50) NOT NULL,
+   email VARCHAR (355) UNIQUE NOT NULL,
+   created_on TIMESTAMP NOT NULL,
+   last_login TIMESTAMP,
+   score VARCHAR (50)
+);
