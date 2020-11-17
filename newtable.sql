760b43bbc478c8257ec64b7d62e9c2a76562913a
gurushyam7 <61306115+gurushyam7@users.noreply.github.com>
2020-11-17 16:47:16 +0530

    Update newtable.sql

--- newtable.sql
+++ newtable.sql
@@ -2,6 +2,7 @@ CREATE TABLE TOP_SALESFORCE_ACCOUNTS(
    user_id serial PRIMARY KEY,
    username VARCHAR (50) UNIQUE NOT NULL,
    password VARCHAR (50) NOT NULL,
+   phone VARCHAR (50) NOT NULL,
    email VARCHAR (355) UNIQUE NOT NULL,
    created_on TIMESTAMP NOT NULL,
    last_login TIMESTAMP,
